90d1f09b99f87429b8e342c3333471db350ca3fa
Luiz Marinelli <luizh0508@gmail.com>
2025-09-30 23:04:34 -0300

    feat: prototype done, seach waiting 300ms debounce

--- type-ahead-search/src/app/components/type-ahead/type-ahead.ts
+++ type-ahead-search/src/app/components/type-ahead/type-ahead.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { catchError, debounceTime, distinctUntilChanged, Observable, of, switchMap } from 'rxjs';
+import { Wikipedia } from '../../services/wikipedia';
 
 @Component({
   selector: 'app-type-ahead',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './type-ahead.html',
-  styleUrl: './type-ahead.scss'
+  styleUrl: './type-ahead.scss',
 })
 export class TypeAhead {
+  searchInput = new FormControl('');
+  searchResults$!: Observable<any[]>;
 
+  private wikipediaService = inject(Wikipedia);
+
+  constructor() {
+    this.searchResults$ = this.searchInput.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: any) => {
+        if (!term || term.trim() === '') {
+          return of([]);
+        }
+
+        return this.wikipediaService.search(term).pipe(
+          catchError((error) => {
+            console.error('Error fetching data', error);
+            return of([]);
+          })
+        );
+      })
+    );
+  }
 }
