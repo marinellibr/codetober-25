9e1e516b1b9d526ee2e577b36b44b8836181f816
Luiz Marinelli <luizh0508@gmail.com>
2025-09-30 23:07:40 -0300

    feat: adding a loading indicator to prototype

--- type-ahead-search/src/app/components/type-ahead/type-ahead.ts
+++ type-ahead-search/src/app/components/type-ahead/type-ahead.ts
@@ -1,7 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { catchError, debounceTime, distinctUntilChanged, Observable, of, switchMap } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  of,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { Wikipedia } from '../../services/wikipedia';
 
 @Component({
@@ -9,16 +17,19 @@ import { Wikipedia } from '../../services/wikipedia';
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './type-ahead.html',
   styleUrl: './type-ahead.scss',
+  standalone: true,
 })
 export class TypeAhead {
   searchInput = new FormControl('');
   searchResults$!: Observable<any[]>;
+  isLoading = false;
 
   private wikipediaService = inject(Wikipedia);
 
   constructor() {
     this.searchResults$ = this.searchInput.valueChanges.pipe(
       debounceTime(300),
+      tap(() => (this.isLoading = true)),
       distinctUntilChanged(),
       switchMap((term: any) => {
         if (!term || term.trim() === '') {
@@ -31,7 +42,9 @@ export class TypeAhead {
             return of([]);
           })
         );
-      })
+      }),
+
+      tap(() => (this.isLoading = false))
     );
   }
 }
